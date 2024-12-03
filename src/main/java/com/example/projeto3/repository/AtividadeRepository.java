f435d0f709bcdecdf2974a0c48382afc0f89ddc0
guleao <guztavusz@gmail.com>
2024-12-03 17:24:41 -0300

    criação dos repositorys

--- src/main/java/com/example/projeto3/repository/AtividadeRepository.java
+++ src/main/java/com/example/projeto3/repository/AtividadeRepository.java
@@ -1,9 +1,13 @@
 package com.example.projeto3.repository;
 
 import com.example.projeto3.model.Atividade;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
+    List<Atividade> findAllByOrderByDataExpiracaoAsc();
 }
